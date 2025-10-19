87279dbf7f0e5792fbd6587ee9331c4aafde3502
emmanueltan <emmanueltan2000@gmail.com>
2025-10-19 22:47:25 +0800

    switch out gtf file to full annotation list

--- scripts/download-references.sh
+++ scripts/download-references.sh
@@ -2,8 +2,10 @@
 
 wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_30/gencode.v30.pc_transcripts.fa.gz
 wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_30/GRCh38.p12.genome.fa.gz
-wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_30/gencode.v30.primary_assembly.annotation.gtf.gz
+# wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_30/gencode.v30.primary_assembly.annotation.gtf.gz
+wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_30/gencode.v30.annotation.gtf.gz
 
 gunzip gencode.v30.pc_transcripts.fa.gz
 gunzip GRCh38.p12.genome.fa.gz
-gunzip gencode.v30.primary_assembly.annotation.gtf.gz
+# gunzip gencode.v30.primary_assembly.annotation.gtf.gz
+gunzip gencode.v30.annotation.gtf.gz
